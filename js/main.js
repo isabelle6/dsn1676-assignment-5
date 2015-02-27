e01145860e4553f2b5a8be15c9ff83ab25b60780
Isabelle6 <isabelle-fortier@hotmail.com>
2015-02-27 16:08:11 -0500

    added form in html added empty ul in html created variables for the form, the list, and the delete button add lis when something is submitted in the form add a button with the lis clear the input field when form is submitted

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var $form = $('.form');
+var $chore = $('.chore');
+var $list = $('.list');
+
+$form.on('submit', function (e) {
+    e.preventDefault();
+    
+    var $li = $('<li>');
+    var $choreP = $('<p>').html($chore.val());
+    var $delete = $('<button>X</button>');
+    
+    $li.append($choreP, $delete);
+    $list.append($li);
+    
+    $form[0].reset();
+});
